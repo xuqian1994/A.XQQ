ae9d8ddfd514a2600f9b2169795a83a43a31572d
android-pf <133300871@qq.com>
2016-08-15 15:07:19 +0800

    add des

--- app/src/main/java/pf/xqq/Main.java
+++ app/src/main/java/pf/xqq/Main.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
 
+    /**
+     * 写在最前面：倩倩想你了。
+     *
+     * 该项目主要用于和倩倩在Github上的协同开发。
+     * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
